0f36601953df34856b088a9505d2cdcb5acaec5a
onlitstechnologies <onlitstechnologies@gmail.com>
2021-04-27 07:00:22 +0530

    20210427

--- C/Array.c
+++ C/Array.c
@@ -1,5 +1,16 @@
-#include<iostream>
+/*
+	PROGRAM TO FIND THE SUM OF TWO INTEGER USING ARRAY	
+*/
+
+#include<stdio.h>
 int main()
 {
-
+	int a[3]; 	//int a, b, c;
+	printf("Enter a number: ");
+	scanf("%d", &a[0]);
+	printf("Enter another number: ");
+	scanf("%d", &a[1]);
+	a[2] = a[0] + a[1];
+	printf("The sum is %d.\n", a[2]);
+	return 0;
 }
