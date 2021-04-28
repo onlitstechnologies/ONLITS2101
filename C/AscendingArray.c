f977d88af5b6556a79c02ba20cf1f1843142a3de
onlitstechnologies <onlitstechnologies@gmail.com>
2021-04-28 06:55:42 +0530

    20210428

--- C/AscendingArray.c
+++ C/AscendingArray.c
@@ -1,2 +1,34 @@
 #include<stdio.h>
+#define SIZE 10		//Symbolic Constant
+int main()
+{
+	int no[SIZE], i , j, t;
+	
+	printf("Enter %d integers below:\n", SIZE);
+	for(i=0; i<SIZE; i++)
+	{
+		scanf("%d", &no[i]);
+	}
+	
+	for(i=0; i<SIZE-1; i++)
+	{
+		for(j=i+1; j<SIZE; j++)
+		{
+			if(no[i]>no[j])
+			{
+				t = no[i];
+				no[i] = no[j];
+				no[j] = t;
+			}
+		}
+	}
+	printf("The integers in ascending order are: ");
+	
+	for(i=0; i<SIZE; i++)
+	{
+		printf("%d, ", no[i]);
+	}
 
+	printf("\n");
+	return 0;
+}
