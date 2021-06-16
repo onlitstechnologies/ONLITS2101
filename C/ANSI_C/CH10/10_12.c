6ba3a6cd17c17f92ed52116c0a3f9e7f85f4c531
onlitstechnologies <onlitstechnologies@gmail.com>
2021-06-16 07:00:14 +0530

    20210616

--- C/ANSI_C/CH10/10_12.c
+++ C/ANSI_C/CH10/10_12.c
@@ -1,36 +1,111 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#define SIZE 5
 struct census
 {
 	char city[50];
 	long int pop;
 	float ll;
-}c[5];
+};
 
+struct census c[SIZE];
+
+void read();
+void sort_alphabetically();
+void sort_population();
+void sort_literacy();
+void display();
+
+int main()
+{
+	read();
+	sort_alphabetically();
+	printf("\nThe city list sorted alphabetically is as follows:\n");
+	display();
+	sort_literacy();
+	printf("\nThe city list sorted literacy level wise is as follows:\n");
+	display();
+	sort_population();
+	printf("\nThe city list sorted population wise is as follows:\n");
+	display();
+	return 0;
+}
 
 void read()
 {
 	int i;
-	printf("Enter the details of 5 cities below:\n");
-	for(i=0; i<1; i++)
+	printf("Enter the details of %d cities below:\n", SIZE);
+	for (i = 0; i < SIZE; i++)
 	{
 		printf("City: ");
 		scanf("%[^\n]", c[i].city);
 		printf("Population: ");
 		scanf("%ld", &c[i].pop);
 		printf("Literacy Level: ");
-		scanf("%f", &c[i].ll);
+		scanf("%f%*c", &c[i].ll);
+	}
+}
+
+void sort_alphabetically()
+{
+	int i, j;
+	struct census t;
+	for (i = 0; i < SIZE - 1; i++)
+	{
+		for (j = i + 1; j < SIZE; j++)
+		{
+			if (strcmp(c[i].city, c[j].city) > 0)
+			{
+				t = c[i];
+				c[i] = c[j];
+				c[j] = t;
+			}
+		}
+	}
+}
+
+void sort_population()
+{
+	int i, j;
+	struct census t;
+	for (i = 0; i < SIZE - 1; i++)
+	{
+		for (j = i + 1; j < SIZE; j++)
+		{
+			if (c[i].pop < c[j].pop)
+			{
+				t = c[i];
+				c[i] = c[j];
+				c[j] = t;
+			}
+		}
+	}
+}
+
+void sort_literacy()
+{
+	int i, j;
+	struct census t;
+	for (i = 0; i < SIZE - 1; i++)
+	{
+		for (j = i + 1; j < SIZE; j++)
+		{
+			if (c[i].ll < c[j].ll)
+			{
+				t = c[i];
+				c[i] = c[j];
+				c[j] = t;
+			}
+		}
 	}
 }
 
 void display()
 {
-	int i = 0;
-	printf("The city list is as follows:\n");
-	printf("%-30s%20ld%10.2f\n",c[i].city, c[i].pop, c[i].ll);
-}
-
-int main()
-{
-	read();
-	display();
+	int i;
+	printf("%-30s%20s%20s\n", "City", "Population", "Literacy Level");
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("%-30s%20ld%20.2f\n", c[i].city, c[i].pop, c[i].ll);
+	}
 }
