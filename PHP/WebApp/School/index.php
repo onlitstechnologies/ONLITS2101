9f1285234815da80d92089b173b33aba42faa6dd
onlitstechnologies <onlitstechnologies@gmail.com>
2021-10-08 14:53:15 +0530

    20211008

---
+++ PHP/WebApp/School/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School Management System</title>
+</head>
+
+<body>
+    <div>
+        <form action="Authenticate.php" method="POST">
+            <label for="user_id">User id</label>
+            <input type="text" name="user_id" id="user_id"> <br><br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password"> <br><br>
+            <button>Login</button>
+        </form>
+    </div>
+</body>
+
+</html>
