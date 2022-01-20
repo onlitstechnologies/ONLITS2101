b6d105f3e6117e65352ec0e978dad960e07835b4
onlitstechnologies <onlitstechnologies@gmail.com>
2022-01-20 13:23:26 +0530

    20220119

--- DS/C/Array/BinarySearch.c
+++ DS/C/Array/BinarySearch.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
+#include "Support.c"
 
 int main()
 {
     int arr[] = {45, 29, 3, 15, 64, 99, 84, 12, 33, 75};
-    int size=10;
-    int value, i, stat=0;
+    int size = 10, mid, lb = 0, ub = size - 1; //lb = lower bound and ub = upper bound
+    int value, i, stat = 0;
     printf("Enter a number to search: ");
     scanf("%d", &value);
     selection_sort(arr, size);
+    while (lb <= ub)
+    {
+        mid = (lb + ub) / 2;
+        if (value == arr[mid])
+        {
+            printf("%d found at index %d.\n", value, mid);
+            stat = 1;
+            break;
+        }
+        if (value < arr[mid])
+        {
+            ub = mid - 1;
+        }
+        else
+        {
+            lb = mid + 1;
+        }
+    }
+    if(stat == 0)
+    {
+        printf("%d not found in the array.\n", value);
+    }
+    return 0;
 }
