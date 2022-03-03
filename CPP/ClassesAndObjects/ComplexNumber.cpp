451cb65964530811e4737a4c7d2fa4789c1c1417
onlitstechnologies <onlitstechnologies@gmail.com>
2022-03-03 08:12:00 +0530

    20220303

--- CPP/ClassesAndObjects/ComplexNumber.cpp
+++ CPP/ClassesAndObjects/ComplexNumber.cpp
@@ -3,14 +3,44 @@ using namespace std;
 
 class complex
 {
-public:                 //violating encapsulation
+private:                 //violating encapsulation
     float real;
-    float imaginary;    
+    float imaginary;
+public:
+    void getdata()
+    {
+        cout << "real: ";
+        cin >> real;
+        cout << "imaginary: ";
+        cin >> imaginary;
+    }
+
+    complex sum(complex c)
+    {
+        complex t;
+        t.real = real + c.real;
+        t.imaginary = imaginary + c.imaginary;
+        return t;
+    }
+    void putdata()
+    {
+        cout <<"real: " << real <<", imaginary: " << imaginary << endl;
+    }
 };
 
 int main()
 {
-    complex c1, c2, c3;
-    cout << "Enter a complex number (r, i): ";
-    cin >> c1.real >> c1.imaginary;
+    complex c1, c2, c3;     //here c1, c2, c3
+    cout << "Enter a complex number: " <<endl;
+    c1.getdata();
+    cout << "Enter another complex number: " << endl;
+    c2.getdata();
+    c3 = c1.sum(c2);
+    cout << "c1: ";
+    c1.putdata();
+    cout << "c2: ";
+    c2.putdata();
+    cout << "c3 (sum): ";
+    c3.putdata();
+    return 0;
 }
