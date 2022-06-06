f034c903f05b87492c3640f50cc13b35d2801f4f
onlitstechnologies <onlitstechnologies@gmail.com>
2022-06-06 16:56:50 +0530

    20220606

---
+++ WWW/Projects/Bank/Employee/dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <div>
+        <header>
+            <span><h1>ABC Bank Ltd.</h1></span>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Customer</a>
+                <a href="">Account</a>
+                <a href="">Logout</a>
+            </nav>
+        </header>
+        <main>
+            <h3>Dashboard</h3>
+        </main>
+        <footer>
+            <span>&copy; 2022, ONLITS TECHNOLOGIES LLP</span>
+        </footer>
+    </div>
+</body>
+</html>
--- WWW/Projects/Bank/Employee/login-script.php
+++ WWW/Projects/Bank/Employee/login-script.php
@@ -1,8 +1,18 @@
 <?php
 require_once("./login-info.php");
 $con = new mysqli($hostname, $user_id, $password, $database);
-if($con->connect_error) {
+if ($con->connect_error) {
     die($con->connect_error);
 }
 
-echo "Connected successfully";
+$email_id = $_POST['email_id'];
+$password = $_POST['password'];
+$sql = "SELECT * FROM employee WHERE email='$email_id' AND password=PASSWORD('$password')";
+
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    header("Location:dashboard.php");
+} else {
+    echo "Invalid email / password.";
+}
